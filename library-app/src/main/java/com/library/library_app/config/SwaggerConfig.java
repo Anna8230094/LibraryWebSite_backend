7bbff33ba04f4cd214490984e44b5af8d43e2386
Anna Megalou <annamegalou3@gmail.com>
2025-03-09 22:40:53 +0200

    Installing swagger UI

---
+++ library-app/src/main/java/com/library/library_app/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.library.library_app.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
