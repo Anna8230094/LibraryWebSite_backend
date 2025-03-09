87f8bd8262adddc1bf0d4cbbe428aa0ac4c1b42a
Anna Megalou <annamegalou3@gmail.com>
2025-03-09 15:34:06 +0200

    Creating spring boot project

---
+++ springboot-library-app/src/test/java/com/library_app/springboot_library_app/SpringbootLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library_app.springboot_library_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
