b48af8bc6504a315b7f275116c878be0a8edabb4
Anna Megalou <annamegalou3@gmail.com>
2025-03-17 18:27:06 +0200

    adding  findByTitleContaining method in BookeRepository

--- library-app/src/main/java/com/library/library_app/dao/BookeRepository.java
+++ library-app/src/main/java/com/library/library_app/dao/BookeRepository.java
@@ -1,11 +1,15 @@
 package com.library.library_app.dao;
 
+import org.springdoc.core.converters.models.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.library.library_app.entity.Book;
 
 @Repository
 public interface BookeRepository extends JpaRepository<Book, Long> {
     
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
